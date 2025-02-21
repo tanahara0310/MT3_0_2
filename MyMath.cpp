005783724b0182d7de63e307c4d011dbb6944ba4
Tanahara Koa <k024g0007@g.neec.ac.jp>
2025-02-22 02:41:37 +0900

    プロジェクト ファイルを追加します。

---
+++ MyMath.cpp
@@ -0,0 +1,39 @@
+﻿#include "MyMath.h"
+#include <math.h>
+
+// ベクトルの加算
+Vector3 Add(const Vector3& v1, const Vector3& v2)
+{
+    return { v1.x + v2.x, v1.y + v2.y, v1.z + v2.z };
+}
+
+// ベクトルの減算
+Vector3 Subtract(const Vector3& v1, const Vector3& v2)
+{
+    return { v1.x - v2.x, v1.y - v2.y, v1.z - v2.z };
+}
+
+// ベクトルのスカラー倍
+Vector3 Multiply(float scalar, const Vector3& v)
+{
+    return { scalar * v.x, scalar * v.y, scalar * v.z };
+}
+
+// ベクトルの内積
+float Dot(const Vector3& v1, const Vector3& v2)
+{
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+// ベクトルの長さ
+float Length(const Vector3& v)
+{
+    return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+// ベクトルの正規化
+Vector3 Normalize(const Vector3& v)
+{
+    float length = Length(v);
+    return { v.x / length, v.y / length, v.z / length };
+}
---
+++ MyMath.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+struct Vector3 {
+    float x, y, z;
+
+    //Vector3 operator+(const Vector3& other) { return { x + other.x, y + other.y , z + other.z}; }
+    //Vector3 operator-(const Vector3& other) { return { x - other.x, y - other.y, z - other.z }; }
+    //Vector3 operator*(const Vector3& other) { return { x * other.x, y * other.y, z * other.z }; }
+    //Vector3 operator/(const Vector3& other) { return { x / other.x, y / other.y , z / other.z}; }
+    //Vector3 operator=(const Vector3& other) { return { x = other.x, y = other.y , z = other.z}; }
+    //Vector3 operator+=(const Vector3& other) { return { x += other.x, y += other.y, z += other.z }; }
+    //Vector3 operator-=(const Vector3& other) { return { x -= other.x, y -= other.y, z -= other.z }; }
+    //Vector3 operator*=(const Vector3& other) { return { x *= other.x, y *= other.y, z *= other.z}; }
+    //Vector3 operator/=(const Vector3& other) { return { x /= other.x, y /= other.y, z /= other.z }; }
+};
+
+//加算
+Vector3 Add(const Vector3& v1, const Vector3& v2);
+
+// 減算
+Vector3 Subtract(const Vector3& v1, const Vector3& v2);
+
+//スカラー倍
+Vector3 Multiply(float scalar, const Vector3& v);
+
+// 内積
+float Dot(const Vector3& v1, const Vector3& v2);
+
+//長さ
+float Length(const Vector3& v);
+
+// 正規化
+Vector3 Normalize(const Vector3& v);
